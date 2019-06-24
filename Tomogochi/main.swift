9ce85cb6a267b30de7c190ebf403212869335fc9
Andrew Batutin <andrey.batutin@dataart.com>
2019-06-24 15:08:17 +0300

    Initial Commit

---
+++ Tomogochi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tomogochi
+//
+//  Created by Andrey Batutin on 6/24/19.
+//  Copyright © 2019 Andrey Batutin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
